c4e10aaae8323504268366ffc4248e6ff863b14b
KlaraWichmann <klara@imagomat.de>
2021-01-19 14:19:04 +0100

    finished comments for hello_world

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,13 +1,13 @@
 // compile in Linux with gcc:
 // g++ hello_world.cpp -lOpenCL
 
-#include "CL/cl.h"                              //including Open-CL header
+#include "CL/cl.h"                              // including Open-CL header
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#define DATA_SIZE   10                          //size of data/ arra with 10 values
-#define MEM_SIZE    DATA_SIZE * sizeof(float)   //size of needed memory to e alloated for data (values are of type float)
+#define DATA_SIZE   10                          // size of data / array with 10 values
+#define MEM_SIZE    DATA_SIZE * sizeof(float)   // size of needed memory to be allocated for data (values are of type float) - in byte
 
 /** source code of kernel as string **/ 
 const char *KernelSource =
@@ -28,11 +28,11 @@ int main (void)
 	cl_device_id	    		device_id = NULL;         	// number which defines the device
 	cl_uint			num_of_platforms = 0,      	// define variable for total number of platforms 
 					num_of_devices = 0;        	// define variable for total number of devices 
-	cl_context 			context;                  	//
-	cl_kernel 			kernel;                   	//
-	cl_command_queue		command_queue;            	//
-	cl_program 			program;                  	//
-	cl_mem				input, output;            	//
+	cl_context 			context;                  	// context (needed for creating command queue)
+	cl_kernel 			kernel;                   	// kernel (central element of operating system)
+	cl_command_queue		command_queue;            	// command queue (queue containing the commands)
+	cl_program 			program;                  	// program (created from kernel source code)
+	cl_mem				input, output;            	// buffer for input + output
 	float				data[DATA_SIZE] =         	// create array of size DATA_SIZE containing values of type float (numbers 1-10)
 					{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 	size_t				global[1] = {DATA_SIZE};  	// create array of size 1 containing value of DATA_SIZE
@@ -40,7 +40,7 @@ int main (void)
 
 	/* 1) */
 
-	// write number of platforms in num_of_platforms, give return statement to err to test if platforms were found
+	// write total number of platforms in num_of_platforms, give return statement to err to test if platforms were found
 	err = clGetPlatformIDs(0, NULL, &num_of_platforms);
 	if (err != CL_SUCCESS)
 	{
@@ -48,7 +48,8 @@ int main (void)
 		return 0;
 	}
 
-	// reserve num_of_platforms memory, //want to get num_of_platforms platform IDs
+	// reserve num_of_platforms memory
+	// want to get num_of_platforms platform IDs, give return statement to err to test if platforms were found
 	platforms = (cl_platform_id *)malloc(num_of_platforms);
 	err = clGetPlatformIDs(num_of_platforms, platforms, NULL);
 	if (err != CL_SUCCESS)
@@ -60,7 +61,7 @@ int main (void)
 	{
 		int nvidia_platform = 0;
 
-		// 
+		// for loop going over all platforms in 'platforms'
 		for (unsigned int i=0; i<num_of_platforms; i++)
 		{
 			// get the name of the current platform
@@ -139,32 +140,37 @@ int main (void)
 
 	/* 2) */
 
-	// 
-	input  = clCreateBuffer (context, CL_MEM_READ_ONLY,	 MEM_SIZE, NULL, &err);
+	// create buffer for in and output
+	// parameter: (context, flag (e.g. CL_MEM_READ_WRITE, CL_MEM_READ_ONLY, ..), size (in byte), host pointer, errorcodde)
+	input  = clCreateBuffer (context, CL_MEM_READ_ONLY, MEM_SIZE, NULL, &err);
 	output = clCreateBuffer (context, CL_MEM_WRITE_ONLY, MEM_SIZE, NULL, &err);
 
-	// 
+	// copy related data from array 'data' in buffer for input 'input'
+	// parameter: (command_queue, buffer, blocking_write, offset, size (in_byte), data pointer, num_events_in_waiting_list, event_waiting_list, 		// event)
 	clEnqueueWriteBuffer(command_queue, input, CL_TRUE, 0, MEM_SIZE, data, 0, NULL, NULL);
 
-	// 
+	// define order of arguments of kernel: test(input, output)
+	// parameter: (kernel, arg_index, arg_size, arg_value)
 	clSetKernelArg(kernel, 0, sizeof(cl_mem), &input);
 	clSetKernelArg(kernel, 1, sizeof(cl_mem), &output);
 
 
 	/* 3)  */
 
-	// 
+	// queueing of kernel in command queue and seperating into sections 
+	// parameter: (command_queue, kernel, work dimension, global_work_offset, global_work_size (in this case 10), local_work_size, 		     	     // num_events_in_waiting_list, event_waiting_list, event)
 	clEnqueueNDRangeKernel (command_queue, kernel, 1, NULL, global, NULL, 0, NULL, NULL);
 
-	// 
+	// waiting for the end of the operation (all commands in command queue are finished)
 	clFinish(command_queue);
 
-	// 
+	// copy result from output buffer 'output' in array 'results'
+	// parameter: (command_queue, buffer, blocking_read, offset, size, ptr, num_events_in_waiting_list, event_waiting_list, event)
 	clEnqueueReadBuffer(command_queue, output, CL_TRUE, 0, MEM_SIZE, results, 0, NULL, NULL);
 
-  //
-  for (unsigned int i=0; i < DATA_SIZE; i++)
-    printf("%f\n", results[i]);
+  	// print values in array 'result' - squared values
+  	for (unsigned int i=0; i < DATA_SIZE; i++)
+    	printf("%f\n", results[i]);
 
 
 	/* 4) */
