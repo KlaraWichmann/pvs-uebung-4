0c843611337ec877d744344ba3aa9b3db76f279c
Jakob Ruckel <jakob@ruckel.de>
2021-01-19 23:44:52 +0100

    clean up comment placeholders

--- matmult.cpp
+++ matmult.cpp
@@ -4,10 +4,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "CL/cl.h"  //
+#include "CL/cl.h"
 
-#define DATA_SIZE 10                        //
-#define MEM_SIZE DATA_SIZE * sizeof(float)  //
+#define DATA_SIZE 10
+#define MEM_SIZE DATA_SIZE * sizeof(float)
 
 /** **/
 const char* KernelSource =
@@ -19,32 +19,32 @@ const char* KernelSource =
 
 /** **/
 int main(void) {
-    cl_int err;                        //
-    cl_platform_id* platforms = NULL;  //
-    char platform_name[1024];          //
-    cl_device_id device_id = NULL;     //
-    cl_uint num_of_platforms = 0,      //
-        num_of_devices = 0;            //
-    cl_context context;                //
-    cl_kernel kernel;                  //
-    cl_command_queue command_queue;    //
-    cl_program program;                //
-    cl_mem input, output;              //
-    float data[DATA_SIZE] =            //
+    cl_int err;
+    cl_platform_id* platforms = NULL;
+    char platform_name[1024];
+    cl_device_id device_id = NULL;
+    cl_uint num_of_platforms = 0,
+        num_of_devices = 0;
+    cl_context context;
+    cl_kernel kernel;
+    cl_command_queue command_queue;
+    cl_program program;
+    cl_mem input, output;
+    float data[DATA_SIZE] =
         {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    size_t global[1] = {DATA_SIZE};  //
-    float results[DATA_SIZE] = {0};  //
+    size_t global[1] = {DATA_SIZE};
+    float results[DATA_SIZE] = {0};
 
     /* 1) */
 
-    //
+
     err = clGetPlatformIDs(0, NULL, &num_of_platforms);
     if (err != CL_SUCCESS) {
         printf("No platforms found. Error: %d\n", err);
         return 0;
     }
 
-    //
+
     platforms = (cl_platform_id*)malloc(num_of_platforms);
     err = clGetPlatformIDs(num_of_platforms, platforms, NULL);
     if (err != CL_SUCCESS) {
@@ -53,9 +53,9 @@ int main(void) {
     } else {
         int nvidia_platform = 0;
 
-        //
+
         for (unsigned int i = 0; i < num_of_platforms; i++) {
-            //
+
             clGetPlatformInfo(platforms[i], CL_PLATFORM_NAME,
                               sizeof(platform_name), platform_name, NULL);
             if (err != CL_SUCCESS) {
@@ -64,14 +64,14 @@ int main(void) {
                 return 0;
             }
 
-            //
+
             if (strstr(platform_name, "NVIDIA") != NULL) {
                 nvidia_platform = i;
                 break;
             }
         }
 
-        //
+
         err = clGetDeviceIDs(platforms[nvidia_platform], CL_DEVICE_TYPE_GPU, 1,
                              &device_id, &num_of_devices);
         if (err != CL_SUCCESS) {
@@ -80,21 +80,21 @@ int main(void) {
         }
     }
 
-    //
+
     context = clCreateContext(0, 1, &device_id, NULL, NULL, &err);
     if (err != CL_SUCCESS) {
         printf("Unable to create context. Error: %d\n", err);
         return 0;
     }
 
-    //
+
     command_queue = clCreateCommandQueue(context, device_id, 0, &err);
     if (err != CL_SUCCESS) {
         printf("Unable to create command queue. Error: %d\n", err);
         return 0;
     }
 
-    //
+
     program = clCreateProgramWithSource(context, 1, (const char**)&KernelSource,
                                         NULL, &err);
     if (err != CL_SUCCESS) {
@@ -102,14 +102,14 @@ int main(void) {
         return 0;
     }
 
-    //
+
     err = clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
     if (err != CL_SUCCESS) {
         printf("Error building program. Error: %d\n", err);
         return 0;
     }
 
-    //
+
     kernel = clCreateKernel(program, "test", &err);
     if (err != CL_SUCCESS) {
         printf("Error setting kernel. Error: %d\n", err);
@@ -118,32 +118,32 @@ int main(void) {
 
     /* 2) */
 
-    //
+
     input = clCreateBuffer(context, CL_MEM_READ_ONLY, MEM_SIZE, NULL, &err);
     output = clCreateBuffer(context, CL_MEM_WRITE_ONLY, MEM_SIZE, NULL, &err);
 
-    //
+
     clEnqueueWriteBuffer(command_queue, input, CL_TRUE, 0, MEM_SIZE, data, 0,
                          NULL, NULL);
 
-    //
+
     clSetKernelArg(kernel, 0, sizeof(cl_mem), &input);
     clSetKernelArg(kernel, 1, sizeof(cl_mem), &output);
 
     /* 3)  */
 
-    //
+
     clEnqueueNDRangeKernel(command_queue, kernel, 1, NULL, global, NULL, 0,
                            NULL, NULL);
 
-    //
+
     clFinish(command_queue);
 
-    //
+
     clEnqueueReadBuffer(command_queue, output, CL_TRUE, 0, MEM_SIZE, results, 0,
                         NULL, NULL);
 
-    //
+
     for (unsigned int i = 0; i < DATA_SIZE; i++)
         printf("%f\n", results[i]);
 
